8b5822aaf4129821be66a73d9c6fe326f23d8782
Azuma413 <hirekatsu0523@gmail.com>
2024-05-04 23:13:56 +0900

    fix code

--- arduino/kadai2.ino
+++ arduino/kadai2.ino
@@ -1,10 +1,68 @@
 #include <Arduino.h>
 #include <Servo.h>
 
+// Arduino課題用のクラス
+class ToolKit {
+  private:
+    Servo servo;
+    int reflector_pin;
+    int volume_pin;
+    int servo_pin;
+    const static int buff_size = 10;
+    int ref_val_list[buff_size];
+    int count = 0;
+  
+  public:
+    ToolKit(int reflector_pin=A0, int volume_pin=A1, int servo_pin=9) {
+      this->reflector_pin = reflector_pin;
+      this->volume_pin = volume_pin;
+      this->servo_pin = servo_pin;
+      Serial.begin(57600);
+      pinMode(volume_pin, INPUT); // 可変抵抗のピンを初期化
+      pinMode(reflector_pin, INPUT); // フォトリフレクタのピンを初期化
+      this->servo.attach(servo_pin); // サーボモータのピンを初期化
+    }
+    // フォトリフレクタのピン番号のゲッター
+    int get_reflector_pin() {
+      return reflector_pin;
+    }
+    // 可変抵抗のピン番号のゲッター
+    int get_volume_pin() {
+      return volume_pin;
+    }
+    // 配列を更新する関数
+    void update_ref_val_list(int ref_val) {
+      if(count < this->buff_size) {
+        ref_val_list[count] = ref_val;
+        count++;
+      }else {
+        count = 0;
+      }
+    }
+    // 平均値を計算する関数
+    int get_average_ref_val() {
+      int ref_val_sum = 0;
+      for (int i = 0; i < this->buff_size; i++) {
+        ref_val_sum += ref_val_list[i];
+      }
+      return ref_val_sum / this->buff_size;
+    }
+    // 値をシリアル出力する関数
+    void print_val(int val1, int val2) {
+      Serial.print(val1);
+      Serial.print(",");
+      Serial.println(val2);
+    }
+    //10bitでサーボの角度を設定する関数
+    void set10bit_angle(int val) {
+      this->servo.write(val / 1024.0 * 180);
+    }
+};
+
 ToolKit toolkit;
 
 void setup() {
-  toolkit = ToolKit(buff_size=10);
+  toolkit = ToolKit();
 }
 
 void loop() {
@@ -19,61 +77,3 @@ void loop() {
   toolkit.print_val(vol_val, average_ref_val);
   delay(100); // 10Hzでループ
 }
-
-// Arduino課題用のクラス
-class ToolKit {
-  private:
-    Servo servo;
-    int reflector_pin;
-    int volume_pin;
-    int servo_pin;
-    int ref_val_list[];
-    int count = 0;
-  
-  public:
-    void ToolKit(int reflector_pin=A0, int volume_pin=A1, int servo_pin=9, int buff_size=10) {
-      this->reflector_pin = reflector_pin;
-      this->volume_pin = volume_pin;
-      this->servo_pin = servo_pin;
-      ref_val_list = new int[buff_size];
-      Serial.begin(57600);
-      pinMode(volume_pin, INPUT); // 可変抵抗のピンを初期化
-      pinMode(reflector_pin, INPUT); // フォトリフレクタのピンを初期化
-      servo.attach(servo_pin); // サーボモータのピンを初期化
-    }
-    // フォトリフレクタのピン番号のゲッター
-    int get_reflector_pin() {
-      return reflector_pin;
-    }
-    // 可変抵抗のピン番号のゲッター
-    int get_volume_pin() {
-      return volume_pin;
-    }
-    // 配列を更新する関数
-    void update_ref_val_list(int ref_val) {
-      if(count < buff_size) {
-        ref_val_list[count] = ref_val;
-        count++;
-      }else {
-        count = 0;
-      }
-    }
-    // 平均値を計算する関数
-    int get_average_ref_val() {
-      int ref_val_sum = 0;
-      for (int i = 0; i < buff_size; i++) {
-        ref_val_sum += ref_val_list[i];
-      }
-      return ref_val_sum / buff_size;
-    }
-    // 値をシリアル出力する関数
-    void print_val(int val1, int val2) {
-      Serial.print(val1);
-      Serial.print(",");
-      Serial.println(val2);
-    }
-    //10bitでサーボの角度を設定する関数
-    void set10bit_angle(int val) {
-      servo.write(val / 1024.0 * 180);
-    }
-};
